9910074ce5ad3a69ca36ad430abeb32abd48704d
andirosad22 <andirosad22@gmail.com>
2023-12-21 13:15:19 +0700

    make responsive for header

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+document.querySelector('header .burger-menu input').addEventListener('click', function(){
+    document.querySelector('header .holadoc-menu').classList.toggle('slide');
+});
+
+// const menuToggle=document.querySelector(' header .burger-menu input');
+// const menu =document.querySelector('.holadoc-menu ul');
+// menuToggle.addEventListener('click',function(){
+//     console.log('berhasil');
+// }); 
